c36c777b700b55d906d0ac60ee82d79623e70795
Козлов Дмитрий Андреевич <DAndKozlov@sberbank.ru>
2023-06-11 14:11:01 +0300

    еуьз

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class App {
 
@@ -9,9 +14,34 @@ public class App {
         return profile.equals("productions");
     }
 
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/template/");
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+
     public static Javalin getApp() {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(config -> {
+//            if (!isProd()) {
+//                config.plugins.enableDevLogging();
+//            }
+            JavalinThymeleaf.init(getTemplateEngine());
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
         return app;
+
     }
 
     public static void main(String[] args) {
---
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -0,0 +1,7 @@
+package hexlet.code.controllers;
+
+import io.javalin.http.Handler;
+
+public class RootController {
+    public static Handler index = ctx -> ctx.render("index.html");
+}
